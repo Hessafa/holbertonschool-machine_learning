ff62e78453146ada4de7988a6b38c9fc829ac178
Hussah ✨ <10685@holbertonstudents.com>
2026-03-10 17:11:07 +0300

    Implement matrix_shape function for nested matrices
    Added a function to calculate the shape of a nested matrix.

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python3
+"""
+Calculates the shape of a nested matrix.
+"""
 
+
+def matrix_shape(matrix):
+    """
+    Calculates the shape of a nested matrix.
+
+    Args:
+        matrix (list): A nested list representing the matrix.
+
+    Returns:
+        list: A list of integers representing the shape of the matrix.
+    """
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
