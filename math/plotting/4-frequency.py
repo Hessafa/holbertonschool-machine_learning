3d113dc3b00389a3cec78954252c71eade79fab9
Hussah ✨ <10685@holbertonstudents.com>
2026-03-10 22:24:10 +0300

    Create frequency.py to plot student grades histogram
    Added a script to plot a histogram of student grades for Project A.

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python3
+"""Plot a histogram of student grades for Project A."""
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """Plot histogram of student grades with bins of 10 and black edges."""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Plot histogram
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor="black")
+
+    # Labels and title
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
