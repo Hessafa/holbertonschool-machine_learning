01183ad8da3f399aefbdc8039155fd3e2112ff8f
Hussah ✨ <10685@holbertonstudents.com>
2026-03-10 17:11:27 +0300

    Implement matrix transpose function
    Add a function to transpose a given matrix.

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python3
+""" Transpose function to check a new matrix transposed"""
 
+
+def matrix_transpose(matrix):
+    """ Return a new matrix transposed
+
+    Args:
+        matrix: given list
+
+    Return:
+        new_matrix: Transposed matrix
+    """
+    new_matrix = [[matrix[j][i] for j in range(len(matrix))]
+                  for i in range(len(matrix[0]))]
+    return new_matrix
