eecd1cced966de9a675f4632dacf6db485efc47a
Hussah ✨ <10685@holbertonstudents.com>
2026-03-10 17:53:02 +0300

    Update 2-from_file.py

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+"""Module that loads data from a file into a pandas DataFrame"""
 
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    """Load data from a file as a pandas DataFrame
+
+    Args:
+        filename (str): file to load
+        delimiter (str): column separator
+
+    Returns:
+        pandas.DataFrame: loaded DataFrame
+    """
+    return pd.read_csv(filename, sep=delimiter)
