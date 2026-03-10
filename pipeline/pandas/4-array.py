66c17c782287102a38c4cd6f857e66ee2fe5ad3c
Hussah ✨ <10685@holbertonstudents.com>
2026-03-10 17:55:13 +0300

    Update 4-array.py

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 """Module that converts selected DataFrame values to a numpy array"""
 
-import pandas as pd
-
 
 def array(df):
     """Select the last 10 rows of High and Close columns as a numpy array
 
     Args:
-        df (pd.DataFrame): DataFrame containing High and Close columns
+        df: pandas DataFrame containing High and Close columns
 
     Returns:
         numpy.ndarray: array of the selected values
