c41475d5628a8d05fbddc5852b87c691a2cc9125
Hussah ✨ <10685@holbertonstudents.com>
2026-03-10 22:27:17 +0300

    Fix indentation for plt.show() in frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -10,6 +10,7 @@ def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
+    # Plot histogram with bins every 10 units and black edges
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor="black")
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
