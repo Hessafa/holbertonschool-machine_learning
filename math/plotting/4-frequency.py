5cc62199030baa5e985e116284c73ffd1dfb8949
Hussah ✨ <10685@holbertonstudents.com>
2026-04-19 14:42:37 +0300

    Refactor frequency function for histogram plotting

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
-"""Plot a histogram of student grades for Project A."""
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def frequency():
-    """Plot histogram of student grades with bins of 10 and black edges."""
+    """Plots a histogram of student grades"""
+
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-    # Plot histogram with bins every 10 units and black edges
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor="black")
-    plt.xlabel("Grades")
-    plt.ylabel("Number of Students")
-    plt.title("Project A")
+
+    # bins every 10 units
+    bins = np.arange(0, 101, 10)
+
+    # plot histogram
+    plt.hist(student_grades, bins=bins, edgecolor='black')
+
+    # labels and title
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+
+    # display plot
     plt.show()
