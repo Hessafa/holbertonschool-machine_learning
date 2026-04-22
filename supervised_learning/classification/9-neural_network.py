c84446042328110313283f011789c984a1ce1150
Hussah ✨ <10685@holbertonstudents.com>
2026-04-22 15:04:37 +0300

    Create NeuralNetwork class for binary classification
    Implement a neural network class with one hidden layer for binary classification, including initialization and private attributes.

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""
+Neural network with one hidden layer (private attributes)
+"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    One hidden layer neural network for binary classification
+    """
+
+    def __init__(self, nx, nodes):
+        """
+        Initialize neural network
+
+        Args:
+            nx (int): number of input features
+            nodes (int): number of hidden nodes
+        """
+
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # Hidden layer
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        # Output layer
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    # -------- getters -------- #
+
+    @property
+    def W1(self):
+        """Weights of hidden layer"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Bias of hidden layer"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Activated output hidden layer"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Weights of output layer"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Bias of output layer"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Activated output (prediction)"""
+        return self.__A2
